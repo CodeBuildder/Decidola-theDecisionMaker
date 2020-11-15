9688735ecab4588610475eed0c728f07c350911c
Kaushik Kumaran <kumarankaushik@gmail.com>
2020-11-15 22:35:02 +0400

    2.0

--- src/components/Decidola.js
+++ src/components/Decidola.js
@@ -5,16 +5,48 @@ import Header from './Header'
 import Options from './Options'
 
 export default class Decidola extends React.Component{
-    constructor(props) {
-        super(props)
-        this.onRemoveAll = this.onRemoveAll.bind(this)
-        this.onRemoveOption = this.onRemoveOption.bind(this)
-        this.randPick = this.randPick.bind(this)
-        this.addOption = this.addOption.bind(this)
-        this.state = {
-            options: []
-        }
+    state = {
+        options:[]
     }
+    randPick= () => {
+        this.setState(() => {
+            
+                const randNum = Math.floor(Math.random() * this.state.options.length);
+                const option = this.state.options[randNum]
+                alert(option)  
+                 
+        })
+    }
+    onRemoveAll= () => {
+        this.setState(() => ({
+        
+            options: []    
+        }))
+    }
+
+    onRemoveOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter ((option) => 
+                 optionToRemove !== option
+            )
+        }))
+    }
+
+    addOption = (option) => {
+
+        if(!option){
+            return 'I am sorry, I quite did not catch that task. Please give a valid task.'
+        }else if(this.state.options.indexOf(option) > -1){
+            return 'Hey, you have already entered this ask. Try another one!'
+        }
+
+        this.setState((prevState) => ({
+           
+                options: prevState.options.concat([option])
+            
+        }))
+    }
+
 
     componentDidMount() {
 
@@ -47,44 +79,7 @@ export default class Decidola extends React.Component{
         console.log('Component Unmounted')
     }
 
-    randPick(){
-        this.setState(() => {
-            
-                const randNum = Math.floor(Math.random() * this.state.options.length);
-                const option = this.state.options[randNum]
-                alert(option)  
-                 
-        })
-    }
-    onRemoveAll() {
-        this.setState(() => ({
-        
-            options: []    
-        }))
-    }
-
-    onRemoveOption(optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter ((option) => 
-                 optionToRemove !== option
-            )
-        }))
-    }
-
-    addOption(option){
-
-        if(!option){
-            return 'I am sorry, I quite did not catch that task. Please give a valid task.'
-        }else if(this.state.options.indexOf(option) > -1){
-            return 'Hey, you have already entered this ask. Try another one!'
-        }
-
-        this.setState((prevState) => ({
-           
-                options: prevState.options.concat([option])
-            
-        }))
-    }
+    
 
     render(){
         const title = 'Decidola'
