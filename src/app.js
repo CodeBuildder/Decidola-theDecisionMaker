faecc36e392851447fa39f99bf080f7ceb0d3d0d
Kaushik Kumaran <kumarankaushik@gmail.com>
2020-11-17 14:42:16 +0400

    2.3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Decidola from './components/Decidola.js'
+import './styles/styles.scss'
+
 
 ReactDOM.render(<Decidola />, document.getElementById('app'))
